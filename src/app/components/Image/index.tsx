4248aa7a90dd3d8950713ce365dc32904abca126
marbokeeffe <mark.okeeffe@gmail.com>
2024-10-21 20:38:33 +0100

    Test classname

--- src/app/components/Image/index.tsx
+++ src/app/components/Image/index.tsx
@@ -16,12 +16,12 @@ type ImageProps = {
 export const Image= ({ clear = true, src, alt='', className, float = 'left', width, height, caption }: ImageProps) => 
 <div style={{ width }} className={clsx( float === 'right' ? `lg:float-right lg:ml-4 ${clear && 'clear-right'}` : `lg:float-left lg:mr-4 ${clear && 'clear-left'}`, 'flex flex-col')}>
 <ImageElem
-src={src}
-alt={alt}
-className={clsx('dark:invert border-4 border-black opacity-100', className)}
-width={width}
-height={height}
-priority
+  src={src}
+  alt={alt}
+  // className={clsx('border-4 border-black', className)}
+  width={width}
+  height={height}
+  priority
 />
 <span className={clsx("block italic text-xs mt-2" )}>{caption}</span>
 </div>
